6577050c267c64607e2657df406f06935c70279e
Denis <den.boltivets@gmail.com>
2022-06-24 22:02:36 +0300

    create AdminLogin

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\AdminNav;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends AdminPageController
+{
+    public function index()
+    {
+        return view('admin.pages.login');
+    }
+
+    public function login(Request $request): RedirectResponse
+    {
+        $request->validate(['login' => 'required|string', 'password' => 'required|string']);
+        $credentials = $request->only('login', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->route(AdminNav::ADMIN_DASHBOARD);
+        }
+
+        return redirect()->route('admin_login');
+    }
+}
