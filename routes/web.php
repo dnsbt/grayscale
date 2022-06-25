4877cb8948b8e9e1b8fbad9162b951d90997b801
Denis <den.boltivets@gmail.com>
2022-06-25 20:51:59 +0300

    create site routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\PageController;
 use App\Models\AdminNav;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [PageController::class, 'index']);
+Route::post('/email', [PageController::class, 'email'])->name('email');
+
+
 Route::prefix('/admin')->group(function () {
 
     Route::middleware(['guest'])->group(function () {
