90d88846ce2c0c9b536f98706e6950477d635f43
Denis <den.boltivets@gmail.com>
2022-06-26 13:40:13 +0300

    clean PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,18 +8,24 @@ use App\Models\SiteConfiguration;
 use App\Services\FileService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-
+use Illuminate\Http\Response;
 
 class PageController extends Controller
 {
     private FileService $fileService;
 
+    /**
+     * @param FileService $fileService
+     */
     public function __construct(FileService $fileService)
     {
         $this->fileService = $fileService;
     }
 
-    public function index()
+    /**
+     * @return Response
+     */
+    public function index(): Response
     {
         $siteConfig = SiteConfiguration::all();
         $config = $siteConfig->pluck('value', 'key')->toArray();
